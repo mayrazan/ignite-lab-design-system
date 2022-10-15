9a5321c6a793dbc50f70573c6d9a69394670f162
Mayra Zanchett Manchein <mayramanchein@yahoo.com.br>
2022-10-15 14:54:45 -0300

    add

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import './styles/global.css';
 
 function App() {
-  return <div className='App'>hello</div>;
+  return (
+    <div className='w-screen h-screen bg-gray-900 flex items-center justify-center text-gray-100'>
+      <h1 className='text-4xl text-white'>Hello World</h1>
+    </div>
+  );
 }
 
 export default App;
